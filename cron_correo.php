9e5f796003ba2166b329b79a780d0036196cd22b
osbendc <osbendc@gmail.com>
2021-01-21 09:37:55 +0100

    correo smtp

--- cron_correo.php
+++ cron_correo.php
@@ -37,42 +37,6 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 
-
-    $mail = new PHPMailer\PHPMailer\PHPMailer();
-    $mail->IsSMTP(); // enable SMTP
-
-    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
-    $mail->Username = "fantasticfy@gmail.com";
-    $mail->Password = "46Wito74.";
-    $mail->SetFrom("demo@demo.com");
-    $mail->Subject = "Test";
-    $mail->Body = "hello";
-    $mail->AddAddress("osbendc@gmail.com");
-    $mail->addCC('hello@witocorp.com');
-     if(!$mail->Send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-     } else {
-        echo "Message has been sent";
-     }
-
-/*
-$to = $emailAdmin; 
-$subject = 'Test email with attachment'; 
-//create a boundary string. It must be unique 
-//so we use the MD5 algorithm to generate a random hash 
-$random_hash = md5(date('r', time())); 
-
-//$headers = "From: fromEmail@test.com"; 
-$headers .= "From: noreply@landingfy-creator.com" . "\r\n" .
-"CC:".$emailCarbon;
-//add boundary string and mime type specification 
-$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\""; 
-
 //define the body of the message. 
 ob_end_clean();//
 ob_start(); //Turn on output buffering 
@@ -93,7 +57,26 @@ Content-Disposition: attachment
 //copy current buffer contents into $message variable and delete current output buffer 
 $message = ob_get_clean(); 
 //send the email 
-$mail_sent = mail( $to, $subject, $message, $headers ); 
-//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
-echo $mail_sent ? "Mail sent" : "Mail failed"; */
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->IsSMTP(); // enable SMTP
+
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "fantasticfy@gmail.com";
+$mail->Password = "46Wito74.";
+$mail->SetFrom("demo@demo.com");
+$mail->Subject = "Test";
+$mail->Body = $message;
+$mail->AddAddress("osbendc@gmail.com");
+$mail->addCC('hello@witocorp.com');
+ if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+    echo "Message has been sent";
+ }
 ?>
