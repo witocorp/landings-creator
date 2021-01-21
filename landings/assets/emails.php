e838d30f2272859d3f65c1fd4b06a4eeea7ba8af
osbendc <osbendc@gmail.com>
2021-01-21 12:08:36 +0100

    integer error id

--- landings/assets/emails.php
+++ landings/assets/emails.php
@@ -18,8 +18,8 @@
 	if($_POST['fnacimiento']){
 		$fnacimiento = $_POST['fnacimiento'];	
 	}
-	$sql = "INSERT INTO emails (id, nombre, apellido, telefono, email, direccion, idLanding,fecha,fnacimiento,opciones)
-	VALUES ('', '".htmlspecialchars($_POST['nombre'])."', '".htmlspecialchars($_POST['apellido'])."', '".
+	$sql = "INSERT INTO emails (nombre, apellido, telefono, email, direccion, idLanding,fecha,fnacimiento,opciones)
+	VALUES ('".htmlspecialchars($_POST['nombre'])."', '".htmlspecialchars($_POST['apellido'])."', '".
 	htmlspecialchars($_POST['telefono'])."', '".htmlspecialchars($_POST['email'])."'
 	, '".htmlspecialchars($_POST['direccion'])."', ".$_POST['idLanding'].", '".date("Y-m-d")."', '".
 	$fnacimiento."','".$opciones."')";
