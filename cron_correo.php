c952c46a8444dadc00177c7785145082edd2b24a
osbendc <osbendc@gmail.com>
2021-01-25 00:35:15 +0100

    no enviar listado vacio

--- cron_correo.php
+++ cron_correo.php
@@ -80,25 +80,27 @@ if ($result->num_rows > 0) {
   echo "0 results";
 }
 $conn->close();
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "fantasticfy@gmail.com";
-$mail->Password = "46Wito74.";
-$mail->SetFrom("noreply@landingfy-creator.com");
-$mail->AddStringAttachment($attachmentH. "" .$attachment, 'emails_list.xls', 'base64', 'application/vnd.ms-excel');
-$mail->Subject = "Emails list";
-$mail->Body = "Emails list";
-$mail->AddAddress($emailAdmin);
-$mail->addCC($emailCarbon);
- if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
-    echo "Message has been sent";
- }
+if($attachment  !== ''){
+  $mail = new PHPMailer\PHPMailer\PHPMailer();
+  $mail->IsSMTP(); // enable SMTP
+  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+  $mail->SMTPAuth = true; // authentication enabled
+  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 465; // or 587
+  $mail->IsHTML(true);
+  $mail->Username = "fantasticfy@gmail.com";
+  $mail->Password = "46Wito74.";
+  $mail->SetFrom("noreply@landingfy-creator.com");
+  $mail->AddStringAttachment($attachmentH. "" .$attachment, 'emails_list.xls', 'base64', 'application/vnd.ms-excel');
+  $mail->Subject = "Emails list";
+  $mail->Body = "Emails list";
+  $mail->AddAddress($emailAdmin);
+  $mail->addCC($emailCarbon);
+   if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+   } else {
+      echo "Message has been sent";
+   }
+}
 ?>
