c4db8f40e179f4b509f58cc54e8bd7585ceec6a3
osbendc <osbendc@gmail.com>
2021-01-26 16:25:51 +0100

    textos legales

--- backend/views/landing/_form.php
+++ backend/views/landing/_form.php
@@ -315,24 +315,7 @@ $this->registerJsFile("@web/editor/js/script.js", ['depends' => 'yii\web\JqueryA
         <footer id="editF">
             <div class="container">
               <div class="row align-items-top text-center text-md-left">
-                <div class="col-12 col-sm-6 col-md-4">
-                  <h3><strong>Country A</strong></h3>
-                  <p>Street Address 52<br>Contact Name</p>
-                  <p>+44 827 312 5002</p>
-                  <p><a href="#">countrya@amazing.com</a></p>
-                </div>
-
-                <div class="col-12 col-sm-6 col-md-4 mt-4 mt-sm-0">
-                  <h3><strong>Country B</strong></h3>
-                  <p>Street Address 100<br>Contact Name</p>
-                  <p>+13 827 312 5002</p>
-                  <p><a href="#">countryb@amazing.com</a></p>
-                </div>
-
-                <div class="col-12 col-md-4 mt-5 mt-md-0 text-md-left">
-                  <h3><strong>About Us</strong></h3>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                </div>
+                <a href="">Conditions générales de vente</a> <a href="">Garantie</a>  <a href="">Informations clients et données personnelles</a>  <a href="">Informations sur les cookies et protection de la vie privée</a>  <a href="">Mentions légales du site</a> 
               </div>
 
               <div class="row mt-5">
