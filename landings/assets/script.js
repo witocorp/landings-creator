d86b93c516788d45baa7f5283b613ade814abbd0
osbendc <osbendc@gmail.com>
2021-01-29 16:29:16 +0100

    color corporativo

--- landings/assets/script.js
+++ landings/assets/script.js
@@ -3,6 +3,9 @@ function validateEmail($email) {
   return emailReg.test( $email );
 }
 $(document).ready(function(){
+	
+	$('.nk-cookie-banner').css({ 'border-color': $("body").data("color")+' !important', 'background': $("body").data("color")+' !important' });     
+    
 	$('footer a, .nk-cookie-banner a').on('click', function(){
 			event.preventDefault();
 			$($(this).attr("href")).modal('show');
